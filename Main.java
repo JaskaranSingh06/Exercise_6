de3c5cb147462d721b1ee418a8f3620fd3401732
Jaskaran Singh <jaskaran9619@gmail.com>
2023-05-08 19:46:56 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main{  
+    public static void main(String args[]){ 
+        String name = "Jaskaran Singh";
+        System.out.println(name); 
+        System.out.println("Hello Java");  
+    }  
+}
